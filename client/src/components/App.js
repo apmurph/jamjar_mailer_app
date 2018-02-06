46db8779bfd34178dbae52e938d37639d8839180
apmurph <alan.murphy@mail.com>
2018-02-05 21:15:27 -0500

    L86 Working Version Client side deployed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,46 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-//import { connect } from 'react-redux';
-//import * as actions from '../actions';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-//import Header from './Header';
-//import Landing from './Landing';
-//import Dashboard from './Dashboard';
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
 //import SurveyNew from './surveys/SurveyNew';
 
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-// class App extends Component {
-//   componentDidMount() {
-//     this.props.fetchUser();
-//   }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-//   render() {
-//     return (
-//       <div className="container">
-//         <BrowserRouter>
-//           <div>
-//             <Header />
-//             <Route exact path="/" component={Landing} />
-//             <Route exact path="/surveys" component={Dashboard} />
-//             <Route path="/surveys/new" component={SurveyNew} />
-//           </div>
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-         <div>
-           <Route path="/" component={Landing} />
-         </div>
-      </BrowserRouter>
-    </div>
-  );
-};
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,40 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Payments from './Payments';
+// import Payments from './Payments';
 
 class Header extends Component {
-  renderContent() {
+ renderContent() {
     switch (this.props.auth) {
       case null:
         return;
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return [
-          <li key="1"><Payments /></li>,
-          <li key="3" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}
-          </li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
-        ];
+        return <li><a href="/api/logout">Logout</a></li>
     }
   }
-
+  
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? '/surveys' : '/'}
-            className="left brand-logo"
+         <div className="nav-wrapper">
+          <Link 
+           to={this.props.auth ? '/surveys' : '/'} 
+           className="left brand-logo"
           >
-            Emaily
+           Jamjar
           </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
-        </div>
+         </div>
       </nav>
     );
   }
@@ -45,3 +39,15 @@ function mapStateToProps({ auth }) {
 }
 
 export default connect(mapStateToProps)(Header);
+
+
+  //     default:
+  //       return [
+  //         <li key="1"><Payments /></li>,
+  //         <li key="3" style={{ margin: '0 10px' }}>
+  //           Credits: {this.props.auth.credits}
+  //         </li>,
+  //         <li key="2"><a href="/api/logout">Logout</a></li>
+  //       ];
+  //   }
+  // }
